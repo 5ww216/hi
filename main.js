95684f9b3452121448e2df19527db5b189afedfa
user <1430241242@qq.com>
2025-11-06 08:57:32 +0800

    feat: 更新主程序，集成工具模块功能

---
+++ main.js
@@ -0,0 +1,6 @@
+const { formatDate, calculateSum } = require('./utils.js');
+
+console.log('=== Git实践项目 ===');
+console.log('当前日期:', formatDate(new Date()));
+console.log('10 + 20 =', calculateSum(10, 20));
+console.log('项目运行成功！');
